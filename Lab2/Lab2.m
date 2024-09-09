8616083bcd5726e516e3c855b6179fcd51045860
jeronstone <jstone14@terpmail.umd.edu>
2024-09-09 17:06:50 -0400

    lab2 3.3

--- Lab2/Lab2.m
+++ Lab2/Lab2.m
@@ -1,6 +1,6 @@
 %% 3.1
 
-[y, Fs] = audioread('Sound_Files\human_voice.wav');
+[y, Fs] = audioread('..\..\2024_Fall_Lab_2\Sound_Files\human_voice.wav');
 t = linspace(0, length(y)/Fs, length(y));
 t = t';
 figure(1)
@@ -17,9 +17,9 @@ size(y)
 
 %% 3.2
 
-m1 = audioread('Sound_Files\M1.wav');
-m2 = audioread('Sound_Files\M2.wav');
-m3 = audioread('Sound_Files\M3.wav');
+m1 = audioread('..\..\2024_Fall_Lab_2\Sound_Files\M1.wav');
+m2 = audioread('..\..\2024_Fall_Lab_2\Sound_Files\M2.wav');
+m3 = audioread('..\..\2024_Fall_Lab_2\Sound_Files\M3.wav');
 
 length(m1)
 length(m2)
@@ -50,3 +50,24 @@ figure(1);
 p = plot(Rxy);
 p.XData = p.XData / 8000;
 p
+
+%% 3.3
+
+[y, Fs] = audioread('..\..\2024_Fall_Lab_2\Sound_Files\Cafe_with_noise.wav');
+t = linspace(0, length(y)/Fs, length(y));
+t = t';
+figure(1)
+plot(t, y)
+
+freq_y = fft(y);
+
+figure(2);
+plot(Fs/length(freq_y)*(0:(length(freq_y)-1)),abs(freq_y))
+
+y_lowpass = lowpass(y, 800, Fs, 'ImpulseResponse','iir');
+sound(y_lowpass, Fs)
+
+ylpfft = fft(y_lowpass);
+
+figure(3);
+plot(Fs/length(ylpfft)*(0:(length(ylpfft)-1)),abs(ylpfft))
